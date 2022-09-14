6ce5fad7432920601ff2cc70e8b56f943ef70912
Sam Rice <samsrice@gmail.com>
2022-09-13 19:33:39 -0700

    Add functional palette class

---
+++ scripts.js
@@ -0,0 +1,102 @@
+class Color {
+  constructor(hex) {
+    this.hex = hex
+    this.isLocked = false
+  };
+};
+
+class Palette {
+  constructor(color1, color2, color3, color4, color5) {
+    this.color1 = color1
+    this.color2 = color2
+    this.color3 = color3
+    this.color4 = color4
+    this.color5 = color5
+    this.id = Date.now()
+  };
+};
+
+
+//--------------------GLOBAL VARIABLES--------------------
+
+
+var randomColor1 = new Color(getRandomHex());
+var randomColor2 = new Color(getRandomHex());
+var randomColor3 = new Color(getRandomHex());
+var randomColor4 = new Color(getRandomHex());
+var randomColor5 = new Color(getRandomHex());
+
+var currentPalette;
+
+//--------------------QUERY SELECTORS--------------------
+
+// var allColorBoxes = document.querySelectorAll(".box-input");
+var colorBox1 = document.querySelector("#box1");
+var colorBox2 = document.querySelector("#box2");
+var colorBox3 = document.querySelector("#box3");
+var colorBox4 = document.querySelector("#box4");
+var colorBox5 = document.querySelector("#box5");
+
+//--------------------EVENT LISTENERS--------------------
+
+window.addEventListener("load", updateBoxInput);
+
+
+
+//--------------------DOM--------------------
+
+
+function updateBoxInput() {
+  colorBox1.setAttribute(background, randomColor1.hex)
+  
+//figure out how to .innerHTML var randomColor1 (for example)
+
+  
+  createNewPalette();
+}
+
+
+
+
+
+//--------------------EVENT HANDLERS--------------------
+
+function createNewPalette() {
+  return currentPalette = new Palette(randomColor1, randomColor2, randomColor3, randomColor4, randomColor5);
+};
+
+
+
+//--------------------MISC FUNCTIONS--------------------
+
+function getRandomHex() {
+  var allDigits = "0123456789ABCDEF";
+  var hexCode = "#";
+  for (var i = 0; i < 6; i++) {
+    hexCode = hexCode + allDigits.charAt(Math.floor(Math.random() * 16));
+  }
+  return hexCode;
+};
+
+
+
+
+
+
+
+
+
+
+//page load querySelector
+
+// var color1 = getRandomHex()
+// x 5
+
+//new palette
+
+
+//update DOM from new instance of palette
+          //document.querySelectorAll(".box-input")
+          //for loop which iterates through node list of boxes for each:
+            //
+
